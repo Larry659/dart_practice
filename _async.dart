5e495ad9905e99095d201e01132d6a6dd35f8181
lanre <larryfunmbi26@gmail.com>
2026-01-14 12:57:00 +0100

    async await

--- _async.dart
+++ _async.dart
@@ -1,12 +1,17 @@
-void main() {
-  fetchPost().then((p) {
-    print(p.name);
-    print(p.userId);
-  });
+void main() async {
+  // fetchPost().then((p) {
+  //   print(p.name);
+  //   print(p.userId);
+  // });
+
+  final post = await fetchPost(); //async await
+  print(post.name);
+  print(post.userId);
 }
 
-Future<Post> fetchPost() {// future explained
-  const delay = Duration(seconds: 3);
+Future<Post> fetchPost() {
+  // future explained
+  const delay = Duration(seconds:5);
   return Future.delayed(delay, () {
     return Post("my post", 123);
   });
