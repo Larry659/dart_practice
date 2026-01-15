a87215be387328efe2d698716ab81bc79f77ec95
lanre <larryfunmbi26@gmail.com>
2026-01-15 16:18:26 +0100

    set methods

--- set.dart
+++ set.dart
@@ -5,5 +5,11 @@ void main(){
   //var names = {"lekan","lanre","Kunle","niyi"};//curly brace indicate set
  Set<String> names = {"lekan","lanre","Kunle","niyi","lekan"};//curly brace indicate set
 
- print(names);
+  
+  print(names.first); //lekan
+  print(names.isEmpty); //false
+  print(names.last); //niyi
+  print(names.contains("richard")); //false
+  print(names.elementAt(3)); //niyi
+  print(names.firstWhere((s) => s.length < 5)); //niyi
 } //var
